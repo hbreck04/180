982ab6f10033779601325c37308d586a27f9fd37
hbreck04 <breckheiha05@uww.edu>
2024-04-03 10:02:20 -0500

    First Fib update.

---
+++ fib.py
@@ -0,0 +1,13 @@
+# This is a python program to calculate the Fibonacci sequence. Create the program to do this.
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+    
+n = int(input("Enter the number of terms: "))
+print("Fibonacci sequence:")
+for i in range(n):
+    print(fibonacci(i))
